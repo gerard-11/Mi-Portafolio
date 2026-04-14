7dda3a1ed207bf6b3d55ab93f1bae5a770000039
Gerardo J. Rito <grrdrito@gmail.com>
2026-04-13 18:44:56 -0600

    semantic tags to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,20 @@ import {TiSocialFacebook} from "react-icons/ti";
 
 export const Header = () => {
     return (
-          <header className="flex justify-between bg-blue-400 p-2 border-b-amber-500 max-h-10">
-              <div className="flex items-center ">
-                  <h1 className="text-sm md:text-xl font-bold">My Personal projects</h1>
+          <header className="flex justify-between bg-blue-400 p-2 border-b border-white max-h-17">
+              <div className="ml-5 flex items-center gap-4">
+                  <figure className="flex items-center justify-center rounded-full bg-blue-600 text-white font-bold w-12 h-12 text-2xl md:w-16 md:h-16 md:text-4xl shadow-md">
+                      G
+                  </figure>
+                  <div>
+                      <h1 className="text-xs md:text-xl font-bold text-gray-800">Gerardo Jesus Rito A.</h1>
+                      <h2 className="text-xs md:text-lg text-gray-600">Software Developer</h2>
+                  </div>
               </div>
-              <div className=" flex">
-                  <ul className=" flex gap-3 text-xl mr-2 items-center">
+              <nav className=" flex">
+                  <ul className=" flex text-xl gap-2 md:gap-5 md:text-2xl mr-2 items-center">
                       <li>
-                          <a href="https://www.facebook.com/gerardojesus.ritoanguiano" className="hover:text-black transition-colors" target="_blank">
+                          <a href="https://www.facebook.com/gerardojesus.ritoanguiano" className="hover:text-black transition-colors " target="_blank">
                               <TiSocialFacebook />
                           </a>
 
@@ -33,7 +39,7 @@ export const Header = () => {
 
                       </li>
                   </ul>
-              </div>
+              </nav>
 
           </header>
 
